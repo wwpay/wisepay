1b0dc3a770ba2b8d9e2593df3b856381f74fb7d3
lucky4694 <lucky4694@gmail.com>
2026-07-23 13:48:33 +0900

    사원으로 로그인 시 패스워드 변경 메뉴가 언어 모드 변경 시 적용되지 않는 문제 수정

--- js/i18n.js
+++ js/i18n.js
@@ -67,6 +67,7 @@ function applyLang() {
   setTxt('t-nav-rates', '保険料率設定', '보험료율 설정');
   setTxt('t-nav-gas', 'データ管理', '데이터 관리');
   setTxt('t-nav-vacation', '有給休暇', '유급 휴가');
+  setTxt('t-nav-changepw', 'パスワード変更', '패스워드 변경');
 
   // vacation page
   setTxt('t-vac-sel-label',     '従業員選択', '사원 선택');
